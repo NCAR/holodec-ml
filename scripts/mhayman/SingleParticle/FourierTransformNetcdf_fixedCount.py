7b3949379477b5e1db130c8826188232daded31f
Matt Hayman <mhayman@ucar.edu>
2020-01-14 17:33:28 -0500

    Script updates

--- scripts/mhayman/SingleParticle/FourierTransformNetcdf_fixedCount.py
+++ scripts/mhayman/SingleParticle/FourierTransformNetcdf_fixedCount.py
@@ -7,9 +7,10 @@ import FourierOpticsLib as FO
 
 
 ds_path = "/scr/sci/mhayman/holodec/holodec-ml-data/"
-ds_name = "synthethic_holograms_v0.nc"
+# ds_name = "synthethic_holograms_v0.nc"
+ds_name = "synthetic_holograms_v02.nc"  # 3 particle data
 ds_base = ds_name.replace(".nc","")
-particle_count = 1
+particle_count = 3
 
 # define the functions to be applied to the fourier transformed data
 ft_func = {'real':np.real,'imag':np.imag,'amplitude':np.abs}
@@ -22,20 +23,33 @@ print("   data type: "+encoded_dtype)
 print("   output operations: ",end="")
 
 with xr.open_dataset(ds_path+ds_name) as ds:
+    # initialize particle training data
+    particle_data = xr.DataArray(np.zeros((4,particle_count,ds['image'].sizes['hologram_number']),dtype=float),
+                                dims=('particle_property','particle_number','hologram_number'),
+                                coords={'particle_property':['x','y','z','d']})
+
     # initialize the image Fourier Transform channels
     image_ft = {}
     for func in ft_func.keys():
         print(func,end=", ")
         image_ft[func] = xr.DataArray(np.zeros(ds['image'].shape,dtype='float32'),
-                            dims=['ysize','xsize','hologram_number'])
+                            dims=ds['image'].dims) # ['ysize','xsize','hologram_number']
 
     print("")
     ft_start_time = datetime.datetime.now()
     # create a variable with for each Fourier Transform operation of each image
-    for im in range(ds['image'].shape[2]):
-        image_ft0 = FO.OpticsFFT(ds['image'][:,:,im]-np.mean(ds['image'][:,:,im]))
+    for im in range(ds['image'].sizes['hologram_number']):
+        # sort the particle data by hologram number
+        particle_index = np.nonzero(ds['hid'].values==im+1)[0]  # find particles associated with this hologram
+        particle_data.loc[{'hologram_number':im}]=ds[particle_data.coords['particle_property']].isel(particle=particle_index).to_array()
+
+        image0 = ds['image'].isel(hologram_number=im) #.transpose('xsize','ysize')
+        image_ft0 = FO.OpticsFFT(image0.values - np.mean(image0.values))
+
         for func in ft_func.keys():
-            image_ft[func][:,:,im] = ft_func[func](image_ft0) / ft_scale[func]
+            image_ft[func].loc[{'hologram_number':im}] = ft_func[func](image_ft0) / ft_scale[func]
+            # image_ft[func][:,:,im] = ft_func[func](image_ft0) / ft_scale[func]
+        print(f"\r{im+1} of {ds['image'].sizes['hologram_number']} holograms completed",end='')
     ft_stop_time = datetime.datetime.now()
     ft_time = (ft_stop_time-ft_start_time).total_seconds()
 
@@ -47,6 +61,7 @@ with xr.open_dataset(ds_path+ds_name) as ds:
 
     # assign FT data to the new xarray DataSet
     ds_ft = ds.assign(image_ft=image_ft)
+    ds_ft = ds_ft.assign(particle_data=particle_data)
 
 # set up encoding for non-floating point data type options
 nckwargs = {}
@@ -58,4 +73,7 @@ if not 'float' in encoded_dtype:
                             '_FillValue':np.iinfo(encoded_dtype).min}}
 
 # write out the FT DataSet
-ds_ft.to_netcdf(ds_path+ds_base+"_ft_ac_"+"_".join(ft_func.keys())+"_"+encoded_dtype+".nc",**nckwargs)
+nc_name = ds_base+"_ft_multipart_"+"_".join(ft_func.keys())+"_"+encoded_dtype+".nc"
+print("saving data to")
+print(ds_path+nc_name)
+ds_ft.to_netcdf(ds_path+nc_name,**nckwargs)
