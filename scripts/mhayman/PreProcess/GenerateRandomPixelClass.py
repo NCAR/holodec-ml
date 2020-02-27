f2e66a0076133602daf26bf23adc0a4f152ff265
Matt Hayman <mhayman@ucar.edu>
2020-02-27 10:06:02 -0500

    Unet script update

--- scripts/mhayman/PreProcess/GenerateRandomPixelClass.py
+++ scripts/mhayman/PreProcess/GenerateRandomPixelClass.py
@@ -3,7 +3,7 @@ Generate randomized pixel data for
 U-net image reconstruction
 Treat the problem as a classification problem
 """
-
+import sys
 import numpy as np
 import xarray as xr
 import datetime
@@ -11,10 +11,17 @@ import copy
 
 import FourierOpticsLib as FO
 
+# set path to local libraries
+dirP_str = '../../../library'
+if dirP_str not in sys.path:
+    sys.path.append(dirP_str)
+
+import ml_utils as ml
+
 ds_path = "/scr/sci/mhayman/holodec/holodec-ml-data/"
 
 Nsets = 5000  # number of training images to generate
-wavelength = 355e-9
+wavelength = 660e-9
 zbins = 5 # number of histogram bins in z
 binary_amplitude = True  # amplitude is binary
 
@@ -32,9 +39,9 @@ zedges = np.linspace(*param_lim['z'],zbins)
 # set the size of the image
 image_dim = {'x':64,
              'y':64,
-             'pixel_width':3e-6}
+             'pixel_width':10e-6}
 
-nc_name = f"image_data_{image_dim['x']}x{image_dim['x']}_{Nsets}count.nc"
+nc_name = f"random_image_data_{image_dim['x']}x{image_dim['x']}_{Nsets}count.nc"
 
 # initialize plane wave definition
 grid = FO.Coordinate_Grid(((image_dim['y']*2,image_dim['x']*2,),
@@ -86,26 +93,31 @@ Generate pixel input data
 loop
 """
 for iholo in range(Nsets):
-    pix_x = np.int(np.round(np.random.rand()*image_dim['x']+image_dim['x']/2))
-    pix_y = np.int(np.round(np.random.rand()*image_dim['y']+image_dim['y']/2))
+    pix_x = np.int(np.random.rand()*image_dim['x'])
+    pix_y = np.int(np.random.rand()*image_dim['y'])
     adata = np.zeros((image_dim['x']*2,image_dim['y']*2))
-    if binary_amplitude:
-        # force amplitude data to be binary
-        adata[pix_x,pix_y] =  1.0
-    else:
+    adata0 = np.zeros((image_dim['x'],image_dim['y']))
+    zposition = np.random.rand()*(param_lim['z'][1]-param_lim['z'][0])+param_lim['z'][0]
+    
+    ml.next_pt((pix_x,pix_y),adata0,0.8,decay=0.9)
+
+    if not binary_amplitude:
         # let amplitude be any number between 0 and 1
-        adata[pix_x,pix_y]  = np.random.rand()*(max(param_lim['amplitude'])-min(param_lim['amplitude']))+min(param_lim['amplitude'])
+        # otherwise the object is binary
+        ipix = np.nonzero(adata0)
+        adata0[ipix] = np.random.rand(len(ipix[0]))*(max(param_lim['amplitude'])-min(param_lim['amplitude']))+min(param_lim['amplitude'])
 
-    # eliminate points outside of the 
-    # actual image
-    adata[:grid.Nx//4,:] = 0
-    adata[-grid.Nx//4:,:] = 0
-    adata[:,:grid.Ny//4] = 0
-    adata[:,-grid.Ny//4:] = 0
+    adata[grid.Nx//4:-grid.Nx//4,grid.Ny//4:-grid.Ny//4] = adata0
+    # # eliminate points outside of the 
+    # # actual image
+    # adata[:grid.Nx//4,:] = 0
+    # adata[-grid.Nx//4:,:] = 0
+    # adata[:,:grid.Ny//4] = 0
+    # adata[:,-grid.Ny//4:] = 0
 
-    zdata = np.zeros((image_dim['x']*2,image_dim['y']*2))
-    zdata[pix_x,pix_y] = np.random.rand()*(param_lim['z'][1]-param_lim['z'][0])+param_lim['z'][0]
-    zdata[adata<=0] = zmiss  # null zdata where there is no amplitude term
+    zdata = np.ones((image_dim['x'],image_dim['y']))*zmiss
+    ipix = np.nonzero(adata0)
+    zdata[ipix] = zposition
 
     """
     Two choices - include multiple scattering by numerically
@@ -134,15 +146,16 @@ for iholo in range(Nsets):
 
     E1 = E0.copy()
 
-    E1.propagate_to(zdata[pix_x,pix_y])
-    E1.field[pix_x,pix_y] *= (1-adata[pix_x,pix_y])
+    E1.propagate_to(zposition)
+    E1.field *= (1-adata)
+    E1.spatial_filter(OpticalTF)
 
     E1.propagate_to(np.max(param_lim['z']))
     image0 = np.abs(E1.field[grid.Nx//4:-grid.Nx//4,grid.Ny//4:-grid.Ny//4])**2
     imageft0 = FO.OpticsFFT(image0)
 
-    labels.loc[{'hologram_number':iholo,'type':'z'}] = zdata[grid.Nx//4:-grid.Nx//4,grid.Ny//4:-grid.Ny//4]
-    labels.loc[{'hologram_number':iholo,'type':'amplitude'}] = adata[grid.Nx//4:-grid.Nx//4,grid.Ny//4:-grid.Ny//4]
+    labels.loc[{'hologram_number':iholo,'type':'z'}] = zdata
+    labels.loc[{'hologram_number':iholo,'type':'amplitude'}] = adata0
     image.loc[{'hologram_number':iholo}] = image0.copy()
     image_ft.loc[{'hologram_number':iholo,'channel':'real'}] = np.real(imageft0)
     image_ft.loc[{'hologram_number':iholo,'channel':'imag'}] = np.imag(imageft0)
