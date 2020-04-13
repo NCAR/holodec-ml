54b464bc812c1b48a030e67419904782fcff2c02
Matt Hayman <mhayman@ucar.edu>
2020-04-13 17:23:55 -0400

    Train on 9 layers

--- scripts/mhayman/ImageProcess/Train_UNET_MultiPlane.py
+++ scripts/mhayman/ImageProcess/Train_UNET_MultiPlane.py
@@ -40,7 +40,7 @@ valid_fraction = 0.1  # fraction of points used for validation
 
 # Training data file
 ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/UNET_image_256x256_5000count_5particles_v02/'   # linux share
-ds_file='UNET_image_256x256_5000count_5particles_v02/UNET_image_256x256_5000count_5particles_9zplanes_v02.nc'
+ds_file='UNET_image_256x256_5000count_5particles_9zplanes_v02.nc'
 
 input_variable = 'image_planes'
 
