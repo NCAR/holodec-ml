ebdee927cb0054c6b31a36cb25e7c2b7c2a837b6
Matt Hayman <mhayman@ucar.edu>
2020-05-23 08:41:50 -0400

    update some evaluation plots

--- scripts/mhayman/ImageProcess/Evaluate_UNET.py
+++ scripts/mhayman/ImageProcess/Evaluate_UNET.py
@@ -55,6 +55,8 @@ index_list = [235,332,841,1078,1398]  # example cases to run
 # ds_file='UNET_image_256x256_5000count_5particles_5zplanes_v02.nc'
 # model_path = '/scr/sci/mhayman/holodec/holodec-ml-data/UNET/models/UNET_Layers6_Conv5_Pool2_Filt32_mse_linear/UNET_image_256x256_5000count_5particles_5zplanes_v02/'
 # model_file = 'UNET_Layers6_Conv5_Pool2_Filt32_mse_linear_epochs201_run1.h5'
+# loss_fun = 'mse'  # definition passed into compiler 
+# loss_str = "mse"  # string representation of loss for filename
 
 # ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/UNET_image_256x256_5000count_5particles_v02/'   # linux share
 # ds_file='UNET_image_256x256_5000count_5particles_9zplanes_v02.nc'
@@ -169,13 +171,13 @@ index_list = [235,332,841,1078,1398]  # example cases to run
 # loss_fun = mldef.filtered_mse  # definition passed into compiler 
 # loss_str = "filtered_mse"  # string representation of loss for filename
 
-# ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/'   # linux share
-# ds_file='UNET_image_256x256_5000count_5particles_v02.nc'
-# model_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/models/UNET_D_Layers5_Conv3_Pool2_Filt32_mse_linear/UNET_image_256x256_5000count_5particles_v02/'
-# # model_file='UNET_D_Layers5_Conv3_Pool2_Filt32_mse_linear_epochs20_run1.h5'
-# model_file='UNET_D_Layers5_Conv3_Pool2_Filt32_mse_linear_epochs50_run2.h5'
-# loss_fun = 'mse'  # definition passed into compiler 
-# loss_str = "mse"  # string representation of loss for filename
+ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/'   # linux share
+ds_file='UNET_image_256x256_5000count_5particles_v02.nc'
+model_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/models/UNET_D_Layers5_Conv3_Pool2_Filt32_mse_linear/UNET_image_256x256_5000count_5particles_v02/'
+# model_file='UNET_D_Layers5_Conv3_Pool2_Filt32_mse_linear_epochs20_run1.h5'
+model_file='UNET_D_Layers5_Conv3_Pool2_Filt32_mse_linear_epochs50_run2.h5'
+loss_fun = 'mse'  # definition passed into compiler 
+loss_str = "mse"  # string representation of loss for filename
 
 # ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/'   # linux share
 # ds_file='UNET_image_256x256_5000count_5particles_v02.nc'
@@ -184,12 +186,12 @@ index_list = [235,332,841,1078,1398]  # example cases to run
 # loss_fun = mldef.filtered_mse  # definition passed into compiler 
 # loss_str = "filtered_mse"  # string representation of loss for filename
 
-ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/'   # linux share
-ds_file='UNET_image_256x256_5000count_5particles_v02.nc'
-model_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/models/UNET_D_Layers6_Conv5_Pool2_Filt32_mse_linear/UNET_image_256x256_5000count_5particles_v02/'
-model_file='UNET_D_Layers6_Conv5_Pool2_Filt32_mse_linear_epochs50_run1.h5'
-loss_fun = 'mse'  # definition passed into compiler 
-loss_str = "mse"  # string representation of loss for filename
+# ds_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/'   # linux share
+# ds_file='UNET_image_256x256_5000count_5particles_v02.nc'
+# model_path='/scr/sci/mhayman/holodec/holodec-ml-data/UNET/models/UNET_D_Layers6_Conv5_Pool2_Filt32_mse_linear/UNET_image_256x256_5000count_5particles_v02/'
+# model_file='UNET_D_Layers6_Conv5_Pool2_Filt32_mse_linear_epochs50_run1.h5'
+# loss_fun = 'mse'  # definition passed into compiler 
+# loss_str = "mse"  # string representation of loss for filename
 
 
 nn_descript = model_file.split('_epochs')[0]
