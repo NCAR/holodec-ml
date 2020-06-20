25d3c1e81b50a1b9f556a0419de9b2f3cec51560
ggantos <ggantos@ucar.edu>
2020-06-20 12:28:09 -0600

    3 particle initial solution

--- library/data.py
+++ library/data.py
@@ -2,7 +2,7 @@ import os
 import xarray as xr
 import numpy as np
 import pandas as pd
-
+from datetime import datetime
 
 
 num_particles_dict = {
@@ -40,26 +40,58 @@ def scale_images(images):
     """Return images with pixel values between 0 and 1"""
     return images.astype(np.float16)/255.
 
+def flatten_dataset(df):
+    columns = ["x1","y1","z1","d1","x2","y2","z2","d2","x3","y3","z3","d3"]
+    data = []
+    for hid in df['hid'].unique():
+        vect = df.loc[df['hid'] == hid].drop(['hid'], axis=1).values
+        data.append(vect.flatten())
+    data = np.vstack(data)
+    df = pd.DataFrame(data=data, columns=columns)
+    return df
+
 def load_scaled_datasets(data_path, num_particles, output_cols, input_scaler):
     """Given the dataset particle numbers, returns scaled training and validation xarrays."""
     
+    beginning = datetime.now()
+    print(f"BEGINNING: {beginning}")
+    startTime = datetime.now()
     print("Loading training and validation data")
     xr_train = open_dataset(data_path, num_particles, 'train')
     xr_valid = open_dataset(data_path, num_particles, 'valid')
+    print(f"\t- time to load datasets: {datetime.now() - startTime}")
     
-    print("Scaling output data")
+    print("\tScaling input data")
+    start_time = datetime.now()
+    scaled_train_inputs = scale_images(xr_train["image"])
+    print(f"\t\tscaled_train_inputs.shape: {scaled_train_inputs.shape}")
+    print(f"\t\t- time to scale train input data: {datetime.now() - startTime}")
+    start_time = datetime.now()
+    scaled_valid_inputs = scale_images(xr_valid["image"])
+    print(f"\t\tscaled_valid_inputs.shape: {scaled_valid_inputs.shape}")
+    print(f"\t\t- time to scale valid input data: {datetime.now() - startTime}")
+    
+    print("\tScaling output data")
+    start_time = datetime.now()
     train_outputs = xr_train[output_cols].to_dataframe()
     valid_outputs = xr_valid[output_cols].to_dataframe()
+    print(f"\t\t- time to slice output data: {datetime.now() - startTime}")
+    
+    print("\tScaling output data")
+    if num_particles == 3:
+        train_outputs = flatten_dataset(train_outputs)
+        valid_outputs = flatten_dataset(valid_outputs)
     
     scaled_train_outputs = pd.DataFrame(input_scaler.fit_transform(train_outputs),
                                         index=train_outputs.index, columns=train_outputs.columns)
-    
+    print(f"\t\tscaled_train_outputs.shape: {scaled_train_outputs.shape}") 
+    print(f"\t\t- time to scale train output data: {datetime.now() - startTime}")
     scaled_valid_outputs = pd.DataFrame(input_scaler.transform(valid_outputs),
                                         index=valid_outputs.index, columns=valid_outputs.columns)
-    
-    print("Scaling input data")
-    scaled_train_inputs = scale_images(xr_train["image"])
-    scaled_valid_inputs = scale_images(xr_valid["image"])
+    print(f"\t\tscaled_valid_outputs.shape: {scaled_valid_outputs.shape}")
+    print(f"\t\t- time to scale valid output data: {datetime.now() - startTime}")
+    end = datetime.now()
+    print(f"END: {end}\nTIME ELAPSED: {end - beginning}")
 
     return scaled_train_inputs, scaled_valid_inputs, scaled_train_outputs, scaled_valid_outputs, input_scaler
 
--- scripts/ggantos/train_conv2d.py
+++ scripts/ggantos/train_conv2d.py
@@ -7,10 +7,11 @@ import argparse
 import yaml
 import os
 from os.path import join, exists
+from datetime import datetime
 
 sys.path.append('../../')
     
-from library.data import load_scaled_datasets
+from library.data import load_scaled_datasets, open_dataset, flatten_dataset
 from library.models import Conv2DNeuralNetwork
 
 
@@ -23,7 +24,7 @@ metrics = {"mae": mean_absolute_error}
 
 
 def main():
-    
+        
     # parse arguments from config/yaml file
     parser = argparse.ArgumentParser(description='Describe a Conv2D neural network for single particle holodec.')
     parser.add_argument("config", help="Path to config file")
@@ -31,7 +32,10 @@ def main():
     with open(args.config) as config_file:
         config = yaml.load(config_file, Loader=yaml.FullLoader)
 
+    data_path = config["data_path"]
     out_path = config["out_path"]
+    num_particles = config["num_particles"]
+    output_cols = config["output_cols"]
     np.random.seed(config["random_seed"])
     if not exists(out_path):
         os.makedirs(out_path)
@@ -43,14 +47,16 @@ def main():
     scaled_valid_inputs, \
     scaled_train_outputs, \
     scaled_valid_outputs, \
-    input_scaler = load_scaled_datasets(config["data_path"],
-                                        config["num_particles"],
-                                        config["output_cols"],
+    input_scaler = load_scaled_datasets(data_path,
+                                        num_particles,
+                                        output_cols,
                                         input_scaler)
     
     # train and save the model
+    model_start = datetime.now()
     mod = Conv2DNeuralNetwork(**config["conv2d_network"])
     mod.fit(scaled_train_inputs.values, scaled_train_outputs.values)
+    print(f"Running model took {datetime.now() - model_start} time")
     print("Saving the model")
     mod.model.save(join(out_path, config["model_name"]+".h5"))
     
@@ -62,24 +68,36 @@ def main():
                                              index=scaled_train_outputs.index,
                                              columns=scaled_train_outputs.columns)
     
-    # calculate error
-    train_error = metrics[config["metric"]](scaled_train_outputs, scaled_pred_train_outputs)
-    valid_error = metrics[config["metric"]](scaled_valid_outputs, scaled_pred_valid_outputs)
-    print(f"Training Error of: {train_error}")
-    print(f"Validation Error of: {valid_error}")
-    
-    # inverse scaler applied to outputs
+    # apply inverse scaler to outputs
+    pred_train_outputs = input_scaler.inverse_transform(scaled_pred_train_outputs)        
     pred_valid_outputs = input_scaler.inverse_transform(scaled_pred_valid_outputs)
-    pred_train_outputs = input_scaler.inverse_transform(scaled_pred_train_outputs)    
+    
+    # calculate error
+    train_outputs = open_dataset(data_path, num_particles, "train")[output_cols].to_dataframe()
+    valid_outputs = open_dataset(data_path, num_particles, "valid")[output_cols].to_dataframe()
+    if len(output_cols) == 5:
+        train_outputs = flatten_dataset(train_outputs)
+        valid_outputs = flatten_dataset(valid_outputs)
+    error = {"train": {}, "valid": {}}
+    for i, var in enumerate(train_outputs.columns):
+        err = mean_absolute_error(train_outputs[var], pred_train_outputs[:,i])
+        error["train"][var] = err
+        print (f"Training error in {var}: ", err)
+    for i, var in enumerate(valid_outputs.columns):
+        err = mean_absolute_error(valid_outputs[var], pred_valid_outputs[:,i])
+        error["valid"][var] = err
+        print (f"Validation error in {var}: ", err)    
     
     # save results
-    print("Saving the data")
-    pd.DataFrame(data={'train_error': train_error}, index=[0]).to_csv(join(out_path, "conv2d_train_error.csv"), index_label="Output")
-    pd.DataFrame(data={'valid_error': valid_error}, index=[0]).to_csv(join(out_path, "conv2d_valid_error.csv"), index_label="Output")
-    pd.DataFrame(data=pred_valid_outputs).to_csv(join(out_path, "pred_valid_outputs.csv"), index_label="index")
-    pd.DataFrame(data=pred_train_outputs).to_csv(join(out_path, "pred_train_outputs.csv"), index_label="index")
-    pd.DataFrame(data=scaled_pred_train_outputs).to_csv(join(out_path, "scaled_pred_train_outputs.csv"), index_label="index")
-    pd.DataFrame(data=scaled_pred_valid_outputs).to_csv(join(out_path, "scaled_pred_valid_outputs.csv"), index_label="index")
+    print("Saving results and config file..")
+    with open(os.path.join(out_path, 'config.yml'), 'w') as f:
+        yaml.dump(config, f)
+    pd.DataFrame.from_dict(error, orient='index').to_csv(join(out_path, "error.csv"),index=False)
+    pd.DataFrame(data=pred_valid_outputs).to_csv(join(out_path, "pred_valid_outputs.csv"), index=False)
+    pd.DataFrame(data=pred_train_outputs).to_csv(join(out_path, "pred_train_outputs.csv"), index=False)
+    pd.DataFrame(data=scaled_pred_train_outputs).to_csv(join(out_path, "scaled_pred_train_outputs.csv"), index=False)
+    pd.DataFrame(data=scaled_pred_valid_outputs).to_csv(join(out_path, "scaled_pred_valid_outputs.csv"), index=False)
+
     return
 
 if __name__ == "__main__":
