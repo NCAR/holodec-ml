12e40ca991a270a22e5b70a4c01e7784bca8eb6d
ggantos <ggantos@ucar.edu>
2020-07-10 07:37:30 -0600

    update for simpler data.py and running 3particle with full dataset

--- library/models.py
+++ library/models.py
@@ -8,7 +8,8 @@ from tensorflow.keras.optimizers import Adam, SGD
 
 class Conv2DNeuralNetwork(object):
     """
-    A Conv2D Neural Network Model that can support arbitrary numbers of layers.
+    A Conv2D Neural Network Model that can support an arbitrary numbers of
+    layers.
 
     Attributes:
         filters: List of number of filters in each Conv2D layer
@@ -17,24 +18,32 @@ class Conv2DNeuralNetwork(object):
         pool_sizes: List of Max Pool sizes
         dense_sizes: Sizes of dense layers
         dense_activation: Type of activation function for dense layers
-        learning_rate: Optimizer learning rate
+        output_activation: Type of activation function for output layer
+        lr: Optimizer learning rate
         optimizer: Name of optimizer or optimizer object.
+        adam_beta_1: Exponential decay rate for the first moment estimates
+        adam_beta_2: Exponential decay rate for the first moment estimates
+        sgd_momentum: Stochastic Gradient Descent momentum
+        decay: Optimizer decay
         loss: Name of loss function or loss object
         batch_size: Number of examples per batch
         epochs: Number of epochs to train
         verbose: Level of detail to provide during training
         model: Keras Model object
     """
-    def __init__(self, filters=(8,), kernel_sizes=(5,), conv2d_activation="relu",
-                 pool_sizes=(4,), dense_sizes=(64,), dense_activation="relu",
-                 lr=0.001, optimizer="adam",  adam_beta_1=0.9, adam_beta_2=0.999,
-                 sgd_momentum=0.9, decay=0, loss="mae", batch_size=32, epochs=2, verbose=0):
+    def __init__(self, filters=(8,), kernel_sizes=(5,),
+                 conv2d_activation="relu", pool_sizes=(4,), dense_sizes=(64,),
+                 dense_activation="relu", output_activation="softmax",
+                 lr=0.001, optimizer="adam", adam_beta_1=0.9,
+                 adam_beta_2=0.999, sgd_momentum=0.9, decay=0, loss="mse",
+                 batch_size=32, epochs=2, verbose=0):
         self.filters = filters
         self.kernel_sizes = [tuple((v,v)) for v in kernel_sizes]
         self.conv2d_activation = conv2d_activation
         self.pool_sizes = [tuple((v,v)) for v in pool_sizes]
         self.dense_sizes = dense_sizes
         self.dense_activation = dense_activation
+        self.output_activation = output_activation
         self.lr = lr
         self.optimizer = optimizer
         self.optimizer_obj = None
@@ -53,22 +62,32 @@ class Conv2DNeuralNetwork(object):
         conv_input = Input(shape=(input_shape), name="input")
         nn_model = conv_input
         for h in range(len(self.filters)):
-            nn_model = Conv2D(self.filters[h], self.kernel_sizes[h], padding="same",
-                              activation=self.conv2d_activation, name=f"conv2D_{h:02d}")(nn_model)
-            nn_model = MaxPool2D(self.pool_sizes[h], name=f"maxpool2D_{h:02d}")(nn_model)
+            nn_model = Conv2D(self.filters[h],
+                              self.kernel_sizes[h],
+                              padding="same",
+                              activation=self.conv2d_activation,
+                              name=f"conv2D_{h:02d}")(nn_model)
+            nn_model = MaxPool2D(self.pool_sizes[h],
+                                 name=f"maxpool2D_{h:02d}")(nn_model)
         nn_model = Flatten()(nn_model)
         for h in range(len(self.dense_sizes)):
-            nn_model = Dense(self.dense_sizes[h], activation=self.dense_activation, name=f"dense_{h:02d}")(nn_model)
-        nn_model = Dense(output_shape, name=f"dense_output")(nn_model)
+            nn_model = Dense(self.dense_sizes[h],
+                             activation=self.dense_activation,
+                             name=f"dense_{h:02d}")(nn_model)
+        nn_model = Dense(output_shape,
+                         activation=self.output_activation,
+                         name=f"dense_output")(nn_model)
         self.model = Model(conv_input, nn_model)
         if self.optimizer == "adam":
-            self.optimizer_obj = Adam(lr=self.lr, beta_1=self.adam_beta_1, beta_2=self.adam_beta_2, decay=self.decay)
+            self.optimizer_obj = Adam(lr=self.lr, beta_1=self.adam_beta_1,
+                                      beta_2=self.adam_beta_2, decay=self.decay)
         elif self.optimizer == "sgd":
-            self.optimizer_obj = SGD(lr=self.lr, momentum=self.sgd_momentum, decay=self.decay)
+            self.optimizer_obj = SGD(lr=self.lr, momentum=self.sgd_momentum,
+                                     decay=self.decay)
         self.model.compile(optimizer=self.optimizer, loss=self.loss)
         self.model.summary()
 
-    def fit(self, x, y):
+    def fit(self, x, y, xv, yv):
         if len(x.shape[1:])==2:
             x = np.expand_dims(x, axis=-1)
         if len(y.shape) == 1:
@@ -77,11 +96,13 @@ class Conv2DNeuralNetwork(object):
             output_shape = y.shape[1]
         input_shape = x.shape[1:]
         self.build_neural_network(input_shape, output_shape)
-        self.model.fit(x, y, batch_size=self.batch_size, epochs=self.epochs, verbose=self.verbose)
-        return
+        self.model.fit(x, y, batch_size=self.batch_size, epochs=self.epochs,
+                       verbose=self.verbose, validation_data=(xv, yv))
+        return self.model.history.history
 
     def predict(self, x):
-        y_out = self.model.predict(np.expand_dims(x.values, axis=-1), batch_size=self.batch_size)
+        y_out = self.model.predict(np.expand_dims(x, axis=-1),
+                                   batch_size=self.batch_size)
         return y_out
 
     def predict_proba(self, x):
---
+++ scripts/ggantos/train_conv2d_zmass.py
@@ -0,0 +1,84 @@
+import sys
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
+from sklearn.metrics import mean_absolute_error
+import pandas as pd
+import numpy as np
+import argparse
+import pickle
+import yaml
+import os
+from os.path import join, exists
+from datetime import datetime
+
+sys.path.append('../../')
+    
+from library.data import load_scaled_datasets
+from library.models import Conv2DNeuralNetwork
+
+
+scalers = {"MinMaxScaler": MinMaxScaler,
+           "MaxAbsScaler": MaxAbsScaler,
+           "StandardScaler": StandardScaler,
+           "RobustScaler": RobustScaler}
+
+metrics = {"mae": mean_absolute_error}
+
+
+def main():
+        
+    # parse arguments from config/yaml file
+    parser = argparse.ArgumentParser(description='Describe a Conv2D nn')
+    parser.add_argument("config", help="Path to config file")
+    args = parser.parse_args()
+    with open(args.config) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+    path_data = config["path_data"]
+    path_save = config["path_save"]
+    if not os.path.exists(path_save):
+        os.makedirs(path_save)
+    num_particles = config["num_particles"]
+    output_cols = config["output_cols"]
+    np.random.seed(config["random_seed"])
+    
+    # load data
+    load_start = datetime.now()
+    output_scaler = scalers[config["output_scaler"]]()
+    train_inputs,\
+    train_outputs,\
+    valid_inputs,\
+    valid_outputs = load_scaled_datasets(path_data,
+                                         num_particles,
+                                         output_cols,
+                                         output_scaler,
+                                         config["subset"],
+                                         config["num_z_bins"])
+    print(f"Loading datasets took {datetime.now() - load_start} time")
+    
+    # train and save the model
+    model_start = datetime.now()
+    mod = Conv2DNeuralNetwork(**config["conv2d_network"])
+    hist = mod.fit(train_inputs, train_outputs,
+                   valid_inputs, valid_outputs)
+    print(f"Running model took {datetime.now() - model_start} time")
+    
+    # predict outputs
+    train_outputs_pred = mod.predict(train_inputs)
+    valid_outputs_pred = mod.predict(valid_inputs) 
+    
+    # save results
+    print("Saving results and config file..")
+    mod.model.save(join(path_save, config["model_name"]+".h5"))
+    name_save = open(join(path_save, "train_outputs_pred.pkl"), 'wb')
+    pickle.dump(train_outputs_pred, name_save)
+    name_save = open(join(path_save, "valid_outputs_pred.pkl"), 'wb')
+    pickle.dump(valid_outputs_pred, name_save)
+    name_save = open(join(path_save, 'hist.pkl'), 'wb')
+    pickle.dump(hist, name_save)
+    with open(join(path_save, 'config.yml'), 'wb') as f:
+        yaml.dump(config, f)
+    
+    return
+
+if __name__ == "__main__":
+    main()
