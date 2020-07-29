a3cdc9c954507ee911c36a03df5efb6cd4de7796
ggantos <ggantos@ucar.edu>
2020-07-29 16:57:21 -0600

    adding bash and yaml files

---
+++ scripts/ggantos/batch_3particle_zmass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -J casp_nb
+#SBATCH --account=NAML0001
+#SBATCH -t 8:00:00
+#SBATCH --mem=256G
+#SBATCH -n 1
+#SBATCH --gres=gpu:v100:1
+#SBATCH -o conv2d.o
+#SBATCH -e conv2d.o
+module load gnu/8.3.0 openmpi/3.1.4 python/3.7.5 cuda/10.1
+ncar_pylib ncar_20200417
+export PATH="/glade/work/ggantos/ncar_20200417/bin:$PATH"
+    
+python train_conv2d_zmass.py ../../config/3particle_zmass.yml
