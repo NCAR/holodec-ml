2ae8624b745a9420b6fe315434e9a84ce5c02d45
Matthew Hayman <mhayman@ucar.edu>
2020-07-30 08:23:39 -0600

    change binsizes to log
    use log bin sizes

--- scripts/mhayman/histogram_estimation/run_preprocess_cheyenne.py
+++ scripts/mhayman/histogram_estimation/run_preprocess_cheyenne.py
@@ -11,7 +11,7 @@ settings = {    'data_file':'synthetic_holograms_1particle_gamma_training.nc',
                 'input_func':{'abs':np.abs},
                 'input_scale':{'abs':255},
                 'FourierTransform':True,
-                'hist_edges':np.linspace(0,300,100)
+                'hist_edges':np.logspace(-1,2.8,100)
                 }
 
 exec_file = 'preprocess_hist_data.py'
