770581a5addf2b0c4057b6b154b7fcb1b0506f5e
Matthew Hayman <mhayman@ucar.edu>
2020-07-30 11:16:37 -0600

    reduce image count
    bus error occured on cheyenne at image 9987

--- scripts/mhayman/histogram_estimation/run_preprocess_cheyenne.py
+++ scripts/mhayman/histogram_estimation/run_preprocess_cheyenne.py
@@ -12,7 +12,7 @@ settings = {    'data_file':'synthetic_holograms_1particle_gamma_training.nc',
                 'input_scale':{'abs':255},
                 'FourierTransform':True,
                 'hist_edges':np.logspace(-1,2.8,100),
-                'max_hist_count':10000
+                'max_hist_count':8000
                 }
 
 exec_file = 'preprocess_hist_data.py'
