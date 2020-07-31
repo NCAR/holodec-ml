1b2a6960f279ad9bad0a1ed5d18e619ecf47da94
Matthew Hayman <mhayman@ucar.edu>
2020-07-31 16:39:56 -0600

    Update run_train_psd_casper.py
    try adding more degrees of freedom and a larger batch size.

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -8,16 +8,16 @@ paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogra
 settings = {
             'data_file':'histogram_training_data_5000count20200731T154352.nc',     # training data file
             'num_epochs':50,    # the number of training epochs
-            'conv_chan':[16,32],# list length defines number of operations
+            'conv_chan':[32,64],# list length defines number of operations
             'conv_size':[5,5],  # convolution kernel size
             'max_pool':[4,4],   # maxpool decimation
-            'nn_size':[64,32,],  # excludes the output layer (set by the input data)
-            'batch_size':64,    # training batch size
-            'output_activation':'linear', # output activation function,
+            'nn_size':[64,64,],  # excludes the output layer (set by the input data)
+            'batch_size':128,    # training batch size
+            'output_activation':'relu', # output activation function,
             'valid_fraction':0.2,   # fraction of data reserved for validation
             'test_fraction':0.3,    # fraction of data reserved for training
             'loss_function':'KLDivergence',     # loss function
-            'h_chunk':64,      # xarray chunk size when loading
+            'h_chunk':128,      # xarray chunk size when loading
             }
 
 exec_file = 'train_psd_convnet.py'
