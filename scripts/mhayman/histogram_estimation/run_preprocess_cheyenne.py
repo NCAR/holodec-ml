c21a537b77c2d1c4d5e59fefda5c8d3f5c789e2e
Matthew Hayman <mhayman@ucar.edu>
2020-08-07 15:48:07 -0600

    use bins matched to the simulated data

--- scripts/mhayman/histogram_estimation/run_preprocess_cheyenne.py
+++ scripts/mhayman/histogram_estimation/run_preprocess_cheyenne.py
@@ -11,7 +11,7 @@ settings = {    'data_file':'synthetic_holograms_1particle_gamma_training.nc',
                 'input_func':{'abs':np.abs},
                 'input_scale':{'abs':255},
                 'FourierTransform':True,
-                'hist_edges':np.logspace(-1,2.8,100),
+                'hist_edges':np.logspace(0,3,40),
                 'max_hist_count':5000
                 }
 
