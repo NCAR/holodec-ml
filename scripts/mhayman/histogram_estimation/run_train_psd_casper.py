c2b253708e815923122b044d79808ab04c056a81
Matthew Hayman <mhayman@ucar.edu>
2020-08-07 09:08:01 -0600

    try bigger conv, mse loss function

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -8,15 +8,15 @@ paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogra
 settings = {
             'data_file':'histogram_training_data_5000count20200731T154352.nc',     # training data file
             'num_epochs':50,    # the number of training epochs
-            'conv_chan':[32,64],# list length defines number of operations
-            'conv_size':[5,5],  # convolution kernel size
+            'conv_chan':[16,32],# list length defines number of operations
+            'conv_size':[11,5],  # convolution kernel size
             'max_pool':[4,4],   # maxpool decimation
             'nn_size':[64,64,],  # excludes the output layer (set by the input data)
             'batch_size':128,    # training batch size
             'output_activation':'relu', # output activation function,
             'valid_fraction':0.2,   # fraction of data reserved for validation
             'test_fraction':0.3,    # fraction of data reserved for training
-            'loss_function':'kstest',     # loss function
+            'loss_function':'mse',     # loss function
             'h_chunk':128,      # xarray chunk size when loading
             }
 
