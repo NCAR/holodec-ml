81108974bb4bea3931b51cc848858f97bf18c147
Matthew Hayman <mhayman@ucar.edu>
2020-08-08 10:08:14 -0600

    use larger images

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -8,9 +8,9 @@ paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogra
 settings = {
             'data_file':'histogram_training_data_10000count20200808T080316.nc',     # training data file
             'num_epochs':50,    # the number of training epochs
-            'conv_chan':[16,32],# list length defines number of operations
+            'conv_chan':[32,32],# list length defines number of operations
             'conv_size':[5,5],  # convolution kernel size
-            'max_pool':[4,4],   # maxpool decimation
+            'max_pool':[2,2],   # maxpool decimation
             'nn_size':[128,64,],  # excludes the output layer (set by the input data)
             'batch_size':128,    # training batch size
             'output_activation':'relu', # output activation function,
