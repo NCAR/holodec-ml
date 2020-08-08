5f8daec0343048a275508008cbab739a26fd704e
Matthew Hayman <mhayman@ucar.edu>
2020-08-08 08:56:11 -0600

    reduce hyperparameters

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -8,8 +8,8 @@ paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogra
 settings = {
             'data_file':'histogram_training_data_10000count20200808T080316.nc',     # training data file
             'num_epochs':20,    # the number of training epochs
-            'conv_chan':[32,64],# list length defines number of operations
-            'conv_size':[11,5],  # convolution kernel size
+            'conv_chan':[16,32],# list length defines number of operations
+            'conv_size':[5,5],  # convolution kernel size
             'max_pool':[4,4],   # maxpool decimation
             'nn_size':[128,64,],  # excludes the output layer (set by the input data)
             'batch_size':128,    # training batch size
