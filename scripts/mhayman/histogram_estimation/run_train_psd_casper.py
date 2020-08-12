aaf96fb47f6cb32bcd6f3cd5c41a46fc6e986c82
Matthew Hayman <mhayman@ucar.edu>
2020-08-11 21:22:25 -0600

    increase kernel and epochs

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -7,9 +7,9 @@ paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogra
 
 settings = {
             'data_file':'histogram_training_data_10000count20200808T080316.nc',     # training data file
-            'num_epochs':100,    # the number of training epochs
+            'num_epochs':200,    # the number of training epochs
             'conv_chan':[32,32],# list length defines number of operations
-            'conv_size':[5,5],  # convolution kernel size
+            'conv_size':[11,5],  # convolution kernel size
             'max_pool':[4,4],   # maxpool decimation
             'nn_size':[128,128,],  # excludes the output layer (set by the input data)
             'batch_size':128,    # training batch size
