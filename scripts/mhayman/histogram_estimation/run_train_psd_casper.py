14c6de8988428b577660f49135760d3e9dd43297
Matthew Hayman <mhayman@ucar.edu>
2020-08-13 09:58:52 -0600

    no maxpool with cumulative poisson

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -10,13 +10,13 @@ settings = {
             'num_epochs':100,    # the number of training epochs
             'conv_chan':[32,32],# list length defines number of operations
             'conv_size':[5,5],  # convolution kernel size
-            'max_pool':[2,2],   # maxpool decimation
+            'max_pool':[1,1],   # maxpool decimation
             'nn_size':[128,64,],  # excludes the output layer (set by the input data)
             'batch_size':256,    # training batch size
             'output_activation':'relu', # output activation function,
             'valid_fraction':0.2,   # fraction of data reserved for validation
             'test_fraction':0.1,    # fraction of data reserved for training
-            'loss_function':'kstest',     # loss function
+            'loss_function':'cum_poisson',     # loss function
             'h_chunk':128,      # xarray chunk size when loading
             'holo_examples':[1, 4, 9, 50, 53, 77, 91, 101, 105, 267]  # example outputs
             }
