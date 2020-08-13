a69acda0613f85918dcc706928fd57967f218f32
Matthew Hayman <mhayman@ucar.edu>
2020-08-13 06:27:35 -0600

    train on new 50-100 particle data

--- scripts/mhayman/histogram_estimation/run_train_psd_casper.py
+++ scripts/mhayman/histogram_estimation/run_train_psd_casper.py
@@ -6,17 +6,17 @@ paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogra
             'save_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/'}
 
 settings = {
-            'data_file':'histogram_training_data_10000count20200808T080316.nc',     # training data file
-            'num_epochs':200,    # the number of training epochs
+            'data_file':'histogram_training_data_10000count20200812T201643.nc',     # training data file
+            'num_epochs':50,    # the number of training epochs
             'conv_chan':[32,32],# list length defines number of operations
-            'conv_size':[11,5],  # convolution kernel size
+            'conv_size':[5,5],  # convolution kernel size
             'max_pool':[4,4],   # maxpool decimation
             'nn_size':[128,128,],  # excludes the output layer (set by the input data)
             'batch_size':128,    # training batch size
             'output_activation':'relu', # output activation function,
-            'valid_fraction':0.2,   # fraction of data reserved for validation
-            'test_fraction':0.3,    # fraction of data reserved for training
-            'loss_function':'cum_poisson',     # loss function
+            'valid_fraction':0.1,   # fraction of data reserved for validation
+            'test_fraction':0.1,    # fraction of data reserved for training
+            'loss_function':'tstest',     # loss function
             'h_chunk':128,      # xarray chunk size when loading
             'holo_examples':[1, 4, 9, 50, 53, 77, 91, 101, 105, 267]  # example outputs
             }
