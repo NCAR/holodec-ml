59bf05c7bbfffb0fa061750547d769d90e2df1bb
Matthew Hayman <mhayman@ucar.edu>
2020-08-13 20:58:42 -0600

    Create radial averaging preprocessor

---
+++ scripts/mhayman/histogram_estimation/run_preprocess_1D_cheyenne.py
@@ -0,0 +1,17 @@
+"""
+Run proprocessing of histogram data on cheyenne
+"""
+
+import numpy as np
+
+paths = {   'data':'/glade/scratch/mhayman/holodec/holodec-ml-data/',
+            'save':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/'}
+
+settings = {    'data_file':'synthetic_holograms_50-100particle_gamma_training.nc',
+                'FourierTransform':True,
+                'hist_edges':np.logspace(0,3,101),
+                'max_hist_count':5000,
+                }
+
+exec_file = 'preprocess_hist_1D_data.py'
+exec(compile(open(exec_file,'rb').read(),exec_file,'exec'))
