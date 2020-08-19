de67b38d470fe098716428437e4146b9c4b77052
Matthew Hayman <mhayman@ucar.edu>
2020-08-18 21:30:32 -0600

    NameError Fix

--- scripts/mhayman/histogram_estimation/train_psd_convnet.py
+++ scripts/mhayman/histogram_estimation/train_psd_convnet.py
@@ -193,8 +193,8 @@ with xr.open_dataset(paths['load_data']+settings['data_file'],chunks={'hologram_
     print(train_data.shape)
 
     print('input scaled data shape')
-    print(scaled_train_data.dims)
-    print(scaled_train_data.shape)
+    print(scaled_train_labels.dims)
+    print(scaled_train_labels.shape)
 
     print('output label shape')
     print(train_labels.dims)
@@ -214,7 +214,7 @@ with xr.open_dataset(paths['load_data']+settings['data_file'],chunks={'hologram_
 
     # build conv NN model
     mod = Sequential()
-    mod.add(Input(shape=scaled_train_data.shape[1:]))
+    mod.add(Input(shape=scaled_train_labels.shape[1:]))
 
     # add convolutional layers
     for ai,n_filters in enumerate(settings['conv_chan']):
