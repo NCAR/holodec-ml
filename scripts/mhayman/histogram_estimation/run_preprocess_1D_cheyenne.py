2398774b39f901ddf00b883659c0dc40fa311a1f
Matthew Hayman <mhayman@ucar.edu>
2020-08-19 09:14:37 -0600

    preprocess bidisperse data

--- scripts/mhayman/histogram_estimation/run_preprocess_1D_cheyenne.py
+++ scripts/mhayman/histogram_estimation/run_preprocess_1D_cheyenne.py
@@ -7,10 +7,9 @@ import numpy as np
 paths = {   'data':'/glade/scratch/mhayman/holodec/holodec-ml-data/',
             'save':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/'}
 
-settings = {    'data_file':['synthetic_holograms_50-100particle_gamma_training.nc',
-                            'synthetic_holograms_50-100particle_gamma_validation.nc',
-                            'synthetic_holograms_50-100particle_gamma_test.nc',
-                            'synthetic_holograms_50-100particle_gamma_private.nc'],
+settings = {    'data_file':['synthetic_holograms_50-100particle_bidisperse_training.nc',
+                            'synthetic_holograms_50-100particle_bidisperse_validation.nc',
+                            'synthetic_holograms_50-100particle_bidisperse_test.nc'],
                 'FourierTransform':True,
                 'hist_edges': np.arange(0,200,10),# np.logspace(0,3,41),
                 'max_hist_count':5000,
