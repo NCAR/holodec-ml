deb9c29a7a91665ca20e565620b87276a8551b69
guwa4340 <guwa4340@colorado.edu>
2020-09-11 16:18:45 -0600

    Added lost files

---
+++ scripts/mhayman/PreProcess/GenerateRandomPixelMultiPlaneMultiLayer.py
@@ -0,0 +1,251 @@
+"""
+Generate randomized pixel data for 
+U-net image reconstruction
+Treat the problem as a classification problem
+"""
+import sys
+import numpy as np
+import xarray as xr
+import dask.array as da
+import datetime
+import copy
+
+
+import FourierOpticsLib as FO
+
+# set path to local libraries
+dirP_str = '../../../library'
+if dirP_str not in sys.path:
+    sys.path.append(dirP_str)
+
+import ml_utils as ml
+
+ds_path = "/scr/sci/mhayman/holodec/holodec-ml-data/"
+
+Nsets = 1000  # number of training images to generate
+wavelength = 355e-9
+zbins = 1 # number of histogram bins in z
+binary_amplitude = True  # amplitude is binary
+
+zmiss = 0  # value for when amplitude is zero
+dspot = 5e-6  # resolvable spot size set by the aperture stop
+Nparticles = 1  # number of particles per hologram
+Prop_Scale = 4  # factor of times bigger propagation grid than detector
+
+h_chunk = 128  # number of holograms in a dask chunk
+
+
+
+# set the randomized space limits
+param_lim = {'z':[0,2e-2],
+             'amplitude':[0.2,1],
+             'Nrange':10,
+             'Nlayer':zbins}
+
+depth_array = np.linspace(param_lim['z'][0],param_lim['z'][1],param_lim['Nrange'])
+
+zedges = np.linspace(*param_lim['z'],zbins)
+
+rspot = dspot/2
+
+layer_list = []
+layer_bins = np.linspace(param_lim['z'][0],param_lim['z'][1],param_lim['Nlayer']+1)
+for ai in range(param_lim['Nlayer']):
+    layer_list +=['amplitude%d'%ai]
+    layer_list +=['z%d'%ai]
+
+# set the size of the image
+image_dim = {'x':256,
+             'y':256,
+             'pixel_width':3e-6}
+
+nc_name = f"random_image_multiplane_data_{image_dim['x']}x{image_dim['x']}_{Nsets}count_{Nparticles}particles_v05.nc"
+
+# initialize simulation grid
+grid = FO.Coordinate_Grid(((image_dim['y']*Prop_Scale,image_dim['x']*Prop_Scale,),
+                           (image_dim['pixel_width'],image_dim['pixel_width'],))
+                           ,inputType='ccd')
+# reconstruction grid
+grid2 = FO.Coordinate_Grid(((image_dim['y'],image_dim['x'],),
+                           (image_dim['pixel_width'],image_dim['pixel_width'],))
+                           ,inputType='ccd')
+# initialize plane wave definition
+E0 = FO.Efield(wavelength,grid,z=np.min(param_lim['z']))
+E0.field = E0.field*0.25
+OpticalTF = E0.grid.fr < 1/rspot
+
+
+ds_attr = {}
+ds_attr['xdim'] = image_dim['x']
+ds_attr['xdim'] = image_dim['y']
+ds_attr['pixel_width'] = image_dim['pixel_width']
+ds_attr['wavelength'] = wavelength
+ds_attr['zmax'] = max(param_lim['z'])
+ds_attr['zmin'] = min(param_lim['z'])
+ds_attr['z_invalid'] = zmiss
+ds_attr['rspot'] = rspot
+ds_attr['dspot'] = dspot
+ds_attr['Nparticles'] = Nparticles
+ds_attr['Nlayer'] = param_lim['Nlayer']
+
+for var in ds_attr:
+    print(var+f":{ds_attr[var]}")
+
+
+xsize = xr.DataArray(np.arange(image_dim['x']),
+                                dims=('xsize'))
+ysize = xr.DataArray(np.arange(image_dim['y']),
+                                dims=('ysize'))
+# depth is actually a combination of real and imaginary components along
+# the z axis
+channel = xr.DataArray(np.arange(depth_array.size*2),
+                                dims=('channel'))
+
+layer_depth = xr.DataArray(0.5*(layer_bins[:-1]+layer_bins[1:]),
+                                dims=('layer_depth'))
+
+# image = xr.DataArray(da.zeros((Nsets,image_dim['x'],image_dim['y'],channel.size),dtype=float),
+#                 dims=('hologram_number','xsize','ysize','channel'),
+#                 coords={'xsize':xsize,'ysize':ysize,'channel':channel})
+
+# image_ft = xr.DataArray(np.zeros((Nsets,image_dim['x'],image_dim['y'],2),dtype=float),
+#                 dims=('hologram_number','xsize','ysize','channel'),
+#                 coords={'xsize':xsize,'ysize':ysize,'channel':['real','imag']})
+
+# labels = xr.DataArray(da.zeros((Nsets,image_dim['x'],image_dim['y'],2),dtype=float),
+#                 dims=('hologram_number','xsize','ysize','type'),
+#                 coords={'xsize':xsize,'ysize':ysize,'type':['z','amplitude'],
+#                         'layer':np.arange(param_lim['Nlayer'])},
+#                 attrs={'description':'pixel properties for training'})
+
+# labels = xr.DataArray(zmiss*da.ones((Nsets,image_dim['x'],image_dim['y'],2*param_lim['Nlayer']),dtype=float),
+#                 dims=('hologram_number','xsize','ysize','layer'),
+#                 coords={'xsize':xsize,'ysize':ysize,'layer':layer_list},
+#                 attrs={'description':'pixel properties for training'})
+
+# labels_a = xr.DataArray(np.zeros((Nsets,image_dim['x'],image_dim['y']),dtype=float),
+#                 dims=('hologram_number','xsize','ysize'),
+#                 coords={'xsize':xsize,'ysize':ysize},
+#                 attrs={'description':'transmission amplitude of the pixel'})
+
+xslice = slice(grid.Nx*(Prop_Scale-1)//(Prop_Scale*2),-grid.Nx*(Prop_Scale-1)//(Prop_Scale*2))
+yslice = slice(grid.Ny*(Prop_Scale-1)//(Prop_Scale*2),-grid.Ny*(Prop_Scale-1)//(Prop_Scale*2))
+
+"""
+Generate pixel input data
+loop
+"""
+for iholo in range(Nsets):
+    
+    labels1 = xr.DataArray(np.zeros((1,image_dim['x'],image_dim['y'],2*param_lim['Nlayer']),dtype=float),
+                dims=('hologram_number','xsize','ysize','layer'),
+                coords={'hologram_number':[iholo],'xsize':xsize,'ysize':ysize,'layer':layer_list},
+                attrs={'description':'pixel properties for training'})
+
+    zposition = sorted(np.random.rand(Nparticles)*(param_lim['z'][1]-param_lim['z'][0])+param_lim['z'][0])
+    E1 = E0.copy()
+    adata = np.ones((image_dim['x']*Prop_Scale,image_dim['y']*Prop_Scale))
+    zdata = np.ones((image_dim['x']*Prop_Scale,image_dim['y']*Prop_Scale))*zmiss
+    
+    for npart in range(Nparticles):
+        # create the particle position
+        pix_x = np.int(np.random.rand()*image_dim['x'])
+        pix_y = np.int(np.random.rand()*image_dim['y'])
+
+        # set up the particle amplitudegrid
+        adatap = np.zeros((image_dim['x']*Prop_Scale,image_dim['y']*Prop_Scale))
+        # adata0 = np.zeros((image_dim['x'],image_dim['y']))
+        
+        # create the random particle
+        ml.next_pt((pix_x+grid.Nx*(Prop_Scale-1)//(Prop_Scale*2), \
+            pix_y+grid.Nx*(Prop_Scale-1)//(Prop_Scale*2)), \
+                adatap,0.8,decay=0.9)
+
+        if not binary_amplitude:
+            # let amplitude be any number between 0 and 1
+            # otherwise the object is binary
+            ipix = np.nonzero(adatap)
+            adatap[ipix] = np.random.rand(len(ipix[0]))*(max(param_lim['amplitude'])-min(param_lim['amplitude']))+min(param_lim['amplitude'])
+
+        # adata[grid.Nx//4:-grid.Nx//4,grid.Ny//4:-grid.Ny//4] = adata0
+
+        # zdata = np.ones((image_dim['x'],image_dim['y']))*zmiss
+        
+        # create the z position array for training
+        ipix = np.nonzero(adatap[xslice,yslice])
+        # ipix = zdata[ipix]
+        # find the layer the particle belongs in
+        ilayer = np.nonzero(zposition[npart]<layer_bins[1:])[0][0]
+        zdata = labels1.sel({'hologram_number':iholo,'layer':'z%d'%ilayer}).values
+        zdata[ipix] = zposition[npart]-layer_bins[ilayer]
+        # labels.sel({'hologram_number':iholo,'layer':'z%d'%ilayer}).values[:,:] = zdata
+        labels1.loc[{'hologram_number':iholo,'layer':'z%d'%ilayer}] = zdata
+        atemp = labels1.sel({'hologram_number':iholo,'layer':'amplitude%d'%ilayer}).values
+        # labels.sel({'hologram_number':iholo,'layer':'amplitude%d'%ilayer}).values[:,:] = 1-(1-atemp)*(1-adatap[xslice,yslice])
+        labels1.loc[{'hologram_number':iholo,'layer':'amplitude%d'%ilayer}] = 1-(1-atemp)*(1-adatap[xslice,yslice])
+
+        # propagate the electric field to the new particle
+        # and apply the particle amplitude mask
+        E1.propagate_to(zposition[npart])
+        E1.field *= (1-adatap)
+        # E1.spatial_filter(OpticalTF)
+
+        # adata *= (1-adatap)
+
+    E1.propagate_to(np.max(param_lim['z']))
+    E1.spatial_filter(OpticalTF)
+
+    image0 = np.abs(E1.field[xslice,yslice])**2
+
+    image1 = xr.DataArray(np.zeros((1,image_dim['x'],image_dim['y'],channel.size),dtype=float),
+                dims=('hologram_number','xsize','ysize','channel'),
+                coords={'hologram_number':[iholo],'xsize':xsize,'ysize':ysize,'channel':channel})
+
+    # adata0 = 1-adata[grid.Nx*(Prop_Scale-1)//(Prop_Scale*2):-grid.Nx*(Prop_Scale-1)//(Prop_Scale*2), \
+    #     grid.Ny*(Prop_Scale-1)//(Prop_Scale*2):-grid.Ny*(Prop_Scale-1)//(Prop_Scale*2)]
+    # zdata0 = zdata[grid.Nx*(Prop_Scale-1)//(Prop_Scale*2):-grid.Nx*(Prop_Scale-1)//(Prop_Scale*2), \
+    #     grid.Ny*(Prop_Scale-1)//(Prop_Scale*2):-grid.Ny*(Prop_Scale-1)//(Prop_Scale*2)]
+    
+    # initialize the reconstruction
+    E2 = FO.Efield(wavelength,grid2,z=E1.z,fielddef=image0)
+    for idepth,depthr in enumerate(depth_array):
+        E2.propagate_to(depthr)
+        image1.sel({'hologram_number':iholo,'channel':2*idepth}).values[:,:] = np.real(E2.field)
+        image1.sel({'hologram_number':iholo,'channel':2*idepth+1}).values[:,:] = np.imag(E2.field)
+        # image.loc[{'hologram_number':iholo,'channel':2*idepth}] = np.real(E2.field)
+        # image.loc[{'hologram_number':iholo,'channel':2*idepth+1}] = np.imag(E2.field)
+    
+    # store the holograms as dask arrays
+    if iholo == 0:
+        image = xr.DataArray(da.from_array(image1.values,chunks=(h_chunk,image_dim['x'],image_dim['y'],channel.size)),
+                    dims=image1.dims,coords=image1.coords)
+        labels = xr.DataArray(da.from_array(labels1.values,chunks=(h_chunk,image_dim['x'],image_dim['y'],2*param_lim['Nlayer'])),
+                    dims=labels1.dims,coords=labels1.coords,attrs=labels1.attrs)
+    else:
+        image = xr.concat([image,image1],dim=('hologram_number'))
+        labels = xr.concat([labels,labels1],dim=('hologram_number'))
+
+
+    # imageft0 = FO.OpticsFFT(image0)
+
+    # labels.loc[{'hologram_number':iholo,'type':'z'}] = zdata0
+    # labels.loc[{'hologram_number':iholo,'type':'amplitude'}] = adata0
+    # image.loc[{'hologram_number':iholo}] = image0.copy()
+    # image_ft.loc[{'hologram_number':iholo,'channel':'real'}] = np.real(imageft0)
+    # image_ft.loc[{'hologram_number':iholo,'channel':'imag'}] = np.imag(imageft0)
+
+    # report progress
+    print(f"\r{iholo+1} of {Nsets} holograms completed",end='')
+
+
+ds = xr.Dataset({'xsize':xsize,'ysize':ysize,'image':image, \
+                'labels':labels,'channel':channel,'layer_depth':layer_depth},
+                attrs=ds_attr)
+
+print("saving data to")
+print(ds_path+nc_name)
+print()
+print("writing...")
+ds.to_netcdf(ds_path+nc_name)
+
+print("write complete")
---
+++ scripts/mhayman/ThreeParticle/FourierTransformHistNetcdf.py
@@ -0,0 +1,126 @@
+"""
+
+"""
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+import datetime
+import FourierOpticsLib as FO
+
+ds_path = "/scr/sci/mhayman/holodec/holodec-ml-data/"
+ds_name = "synthetic_holograms_v02.nc"  # 3 particle data
+#ds_name = "synthethic_holograms_v0.nc"  # 1 particle data
+ds_base = ds_name.replace(".nc","")
+
+# define the functions to be applied to the fourier transformed data
+# this sets the out channels
+# typical names:
+# 'real':np.real, 'imag':np.imag, 'amplitude':np.abs, 'phase':np.angle
+ft_func = {'real':np.real,'imag':np.imag,}
+encoded_dtype = "float32"
+
+# define the data products desired and the number of histogram bins
+# this defines the output histogram dimensions
+hist_bin_count = {"x":51,
+                  "y":51,
+                  }
+
+# general definitions for rescaling factors
+ft_scale = {'real':255,'imag':255,'amplitude':255,'phase':2*np.pi}  # rescaling factors
+
+print("Fourier Transforming: "+ds_name)
+print("   path: "+ds_path)
+print("   data type: "+encoded_dtype)
+print("   output operations: ",end="")
+
+with xr.open_dataset(ds_path+ds_name) as ds:
+    # initialize the particle property histogram bins
+    hist_bin_centers = {}
+    hist_bin_edges = {}
+    histogram_shape = []
+    histogram_size = 1
+    hist_dims = ['hologram_number']
+    hist_coords = [ds['hologram_number']]
+    for param in hist_bin_count.keys():
+        hist_bin_edges[param+'_bin_edges'] = np.linspace(ds[param].min(),ds[param].max(),
+                            hist_bin_count[param])
+        hist_bin_centers[param+'_bin_centers'] = 0.5*(hist_bin_edges[param+'_bin_edges'] [:-1]+
+                            hist_bin_edges[param+'_bin_edges'][1:])
+        histogram_shape+=[hist_bin_count[param]-1]
+        histogram_size *= (hist_bin_count[param]-1)
+
+
+    # initialize the image Fourier Transform channels
+    image_ft = {}
+    for func in ft_func.keys():
+        image_ft[func] = xr.DataArray(np.zeros(ds['image'].transpose("hologram_number", "xsize", "ysize").shape,dtype='float32'),
+                            dims=['hologram_number','xsize','ysize'])
+        print(func,end=", ")
+    print()
+
+    print("   histogram bins: ")
+    print("      "+hist_bin_count.__str__())
+
+    # initialize the histograms
+    particle_histogram = xr.DataArray(np.zeros([ds['hologram_number'].size]+histogram_shape,dtype=np.int8),
+                                coords=[ds['hologram_number']]+list(hist_bin_centers.values()),
+                                dims=['hologram_number']+list(hist_bin_centers.keys()))
+
+
+    ds_ft = ds.assign(**hist_bin_edges)     # create a new data set with histogram bin edges
+    ds_ft = ds_ft.assign_coords(**hist_bin_centers) # create dimension for histogram bin centers
+    ds_ft = ds_ft.assign(particle_histogram=particle_histogram) # add the histogram to the data set
+    
+
+
+    # store the Fourier Transform and particle size histogram for each hologram
+    print("Performing Fourier Transform")
+    ft_start_time = datetime.datetime.now()
+    for im in ds_ft['hologram_number'].values:
+        # find the particles in this hologram
+        # hologram indexing is base 1
+        particle_index = np.nonzero(ds_ft['hid'].values==im+1)[0]  
+        
+        # make a histogram of particles and store it in the data set
+        hist0 = np.histogramdd(ds_ft[list(hist_bin_count.keys())].to_array()[:,particle_index].T,
+                    bins=list(hist_bin_edges.values()))
+        ds_ft['particle_histogram'][im,...] = hist0[0]
+        
+        # FT the image and store the desired operations
+        image0 = ds['image'].sel(hologram_number=im).transpose("xsize", "ysize")  # select the hologram image
+        image_ft0 = FO.OpticsFFT(image0-np.mean(image0))  # FFT the image
+        # perform requested operations for storage
+        for func in ft_func.keys():
+            image_ft[func][im,:,:] = ft_func[func](image_ft0) / ft_scale[func]
+ft_stop_time = datetime.datetime.now()
+
+image_ft = xr.concat(list(image_ft.values()),pd.Index(ft_func.keys(),name='channel'))
+image_ft = image_ft.transpose("hologram_number", "xsize", 'ysize',"channel")  # fix the axis order
+ds_ft = ds_ft.assign(image_ft=image_ft)
+
+ft_time = (ft_stop_time-ft_start_time).total_seconds()
+print(f"{ds['image'].shape[2]} samples Fourier Transformed in {ft_time} seconds")
+print(f"for {ft_time/ds['image'].shape[2]} seconds per hologram")
+
+# set up encoding for non-floating point data type options
+nckwargs = {}
+if not 'float' in encoded_dtype:
+    nckwargs['encoding'] = {'image_ft':{'dtype':encoded_dtype,
+                            'scale_factor':(image_ft.max()-image_ft.min()).values/
+                                (np.iinfo(encoded_dtype).max-np.iinfo(encoded_dtype).min),
+                            'add_offset':image_ft.mean().values,
+                            '_FillValue':np.iinfo(encoded_dtype).min,
+                            'zlib':True,
+                            'complevel':1}}
+else:
+    nckwargs['encoding'] = {'image_ft':{'zlib':True,'complevel':1}}
+# histogram encoding level    
+nckwargs['encoding']['particle_histogram'] = {'zlib':True,'complevel':1}
+
+# write out the FT DataSet
+print("Writing to netcdf")
+ds_ft.to_netcdf(ds_path+ds_base+"_ft_ac_"+
+    "".join(hist_bin_count.keys())+"_"+
+    "_".join(ft_func.keys())+"_"+
+    encoded_dtype+"_histogram.nc",**nckwargs)
