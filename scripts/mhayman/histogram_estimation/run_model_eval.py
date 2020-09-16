9d9a072b74d939f97b5f182e2f4b0e9ff2680ee6
Matthew Hayman <mhayman@ucar.edu>
2020-09-16 10:10:27 -0600

    try kstest training data on gamma

--- scripts/mhayman/histogram_estimation/run_model_eval.py
+++ scripts/mhayman/histogram_estimation/run_model_eval.py
@@ -4,12 +4,12 @@ Train ConvNet to estimate PSD on Casper
 
 paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/',
             'save_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/',
-            'model_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/histogram_training_data_5000count20200819T091551_HypOpt_NN_20200831T093951/'}
-
+            'model_data': '/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/histogram_training_data_5000count20200819T091551_HypOpt_NN_20200916T075255/'# '/glade/scratch/mhayman/holodec/holodec-ml-data/histogram/histogram_training_data_5000count20200819T091551_HypOpt_NN_20200831T093951/'
+        }
 settings = {
             'data_file': 'histogram_training_data_5000count20200819T091551.nc',
             'test_file':'histogram_test_data_5000count20200915T155004.nc',
-            'model_file':'histogram_training_data_5000count20200819T091551_HypOpt_NN_20200831T093951.h5',
+            'model_file': 'histogram_training_data_5000count20200819T091551_HypOpt_NN_20200916T075255.h5',# 'histogram_training_data_5000count20200819T091551_HypOpt_NN_20200831T093951.h5',
             'scale_labels':False, # Scale the labels - depending on loss function, may not be desirable
             'loss_function':'cum_poisson',     # loss function
             'log_input':True,     # include log of the input data as input
