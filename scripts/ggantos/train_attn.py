0c8be80018a386fe08fc6f5e7bc2fd7c95f9346d
djgagne@ou.edu <djgagne@ou.edu>
2020-09-30 16:22:22 -0600

    Updated attention script to address config issues

--- scripts/ggantos/train_attn.py
+++ scripts/ggantos/train_attn.py
@@ -11,7 +11,8 @@ import os
 from os.path import join, exists
 from datetime import datetime
 import tensorflow as tf
-    
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import binary_crossentropy
 from holodecml.data import load_scaled_datasets
 from holodecml.models import ParticleAttentionNet
 
@@ -32,6 +33,17 @@ metrics = {"mae": mean_absolute_error,
            "rmse": rmse,
            "r2": r2,
            "max_error": max_error}
+
+
+def attention_net_loss(y_true, y_pred):
+    # y_true and y_pred will have shape (batch_size x max_num_particles x 5)
+    loss_real = tf.reduce_mean(tf.abs(y_true[y_true[:, :, -1] > 0] - y_pred[y_true[:, :, -1] > 0]))
+    batch_size = tf.shape(y_true)[0]
+    column_count = tf.shape(y_true)[1]
+    loss_bce = binary_crossentropy(y_true[:,:,-1], 
+                                   y_pred[:,:,-1])
+    loss_total = loss_real + loss_bce
+    return loss_total
 
 def main():
         
@@ -71,10 +83,10 @@ def main():
     train_outputs_noisy = train_outputs * (1 + np.random.normal(0, config['noisy_sd'], train_outputs.shape))
     valid_outputs_noisy = valid_outputs * (1 + np.random.normal(0, config['noisy_sd'], valid_outputs.shape))
     
-    net = ParticleAttentionNet()
-    net.compile(optimizer=config['optimizer'], loss=config['loss'])
-    net.fit([train_outputs_noisy, train_inputs], train_outputs, epochs=config['epochs'],
-            batch_size=config['batch_size'], verbose=config['verbose'])
+    net = ParticleAttentionNet(**config["attention_network"])
+    net.compile(optimizer=Adam(lr=config["train"]['learning_rate']), loss=attention_net_loss)
+    net.fit([train_outputs_noisy, train_inputs], train_outputs, epochs=config["train"]['epochs'],
+            batch_size=config["train"]['batch_size'], verbose=config["train"]['verbose'])
     print(f"Running model took {datetime.now() - model_start} time")
     val_outputs_pred_scaled = net.predict([valid_outputs_noisy, valid_inputs], batch_size=config["batch_size"] * 4)
     val_outputs_pred_raw = scaler_out.inverse_transform(val_outputs_pred_scaled)
@@ -95,6 +107,7 @@ def main():
     valid_pred_scaled_da.to_netcdf(join(path_save, "valid_pred_scaled.nc"))
     valid_pred_raw_da.to_netcdf(join(path_save, "valid_pred_raw.nc"))
 
-
+if __name__ == "__main__":
+    main()
     
     
