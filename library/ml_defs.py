d939d74011c1c198896a50b292b5e4044827c208
Matthew Hayman <mhayman@ucar.edu>
2020-10-23 11:31:01 -0600

    Add VAE functions

--- library/ml_defs.py
+++ library/ml_defs.py
@@ -8,6 +8,7 @@ Matthew Hayman
 mhayman@ucar.edu
 """
 
+import numpy as np
 
 from tensorflow.keras.layers import Activation, MaxPool2D, SeparableConv2D, concatenate, Conv2DTranspose,Flatten,Lambda, Conv2D,Dense, Reshape
 import tensorflow.keras.backend as K
@@ -174,6 +175,102 @@ def add_unet_dense(input_node,n_layers,n_filters,nConv=5,
     
     # return the result to the next layer up
     return act_2u
+
+def add_unet_vae(input_node,n_layers,n_filters,nConv=5,
+            nPool=4,activation="relu",kernel_initializer = "he_normal",
+            latent_dim=64,n_dense_layers=2,):
+    """
+    Recursive function for defining a encoding/decoding UNET
+    input_node - the input supplied to the UNET
+    n_layers - the number of desired layers in the UNET
+    n_filters - the number of convolution filters in the first layer
+        this will grow by factors of two for each additional layer depth
+    nConv - number of points in each convolution kernel
+    nPool - number of points in each max-pool operation
+    activation - activation function to use.  Typically 'relu'.
+    latent_dim - number of neurons in the latent dense layers
+    n_dense_layers - number of dense layers used to estimate the latent space 
+
+
+    Example use:
+    # UNET parameter definitions
+    nFilters = 16
+    nPool = 4
+    nConv = 5
+    nLayers = 4
+    loss_fun = "mse"
+    out_act = "linear" 
+
+    # define the input based on input data dimensions
+    cnn_input = Input(shape=scaled_in_data.shape[1:])  
+
+    # create the unet
+    unet_out = add_unet_layers(cnn_input,nLayers,nFilters,nConv=nConv,nPool=nPool,activation="relu")
+
+    # add the output layer
+    out = Conv2D(scaled_train_labels.sizes['type'],(1,1),padding="same",activation=out_act)(unet_out)
+
+    # build and compile the model
+    mod = Model(cnn_input, unet_out)
+    mod.compile(optimizer="adam", loss=loss_fun,metrics=['acc'])
+    mod.summary()
+
+    """
+    
+    if n_layers > 1:
+        # another layer will be created below this one
+        
+        # define the down sampling layer
+        conv_1d = Conv2D(n_filters, (nConv, nConv), padding="same", kernel_initializer = kernel_initializer)(input_node)
+        act_1d = Activation("relu")(conv_1d)
+        conv_2d = Conv2D(n_filters, (nConv, nConv), padding="same", kernel_initializer = kernel_initializer)(act_1d)
+        act_2d = Activation("relu")(conv_2d)
+        pool = MaxPool2D(pool_size=(nPool, nPool))(act_2d)
+
+        # create the next layer below this one
+        return_node = add_unet_vae(pool,n_layers-1,n_filters*2,nConv=nConv,nPool=nPool,activation=activation,
+                    latent_dim=latent_dim,n_dense_layers=n_dense_layers)
+
+        # define the up sampling and feed foward layer
+        upsamp_1u = Conv2DTranspose(n_filters, (nConv,nConv), strides=(nPool,nPool),padding="same")(return_node)
+        # concat_1u = concatenate([upsamp_1u,act_2d],axis=3)  # feed forward - not implemented in VAE
+        conv_1u = Conv2D(n_filters,(nConv,nConv),padding="same",kernel_initializer = kernel_initializer)(upsamp_1u)
+        act_1u = Activation("relu")(conv_1u)
+        conv_2u = Conv2D(n_filters,(nConv,nConv),padding="same",kernel_initializer = kernel_initializer)(act_1u)
+        act_2u = Activation("relu")(conv_2u)
+    else:
+
+        input_shape = K.int_shape(input_node)
+        zinput = Flatten()(input_node)
+        z_mean = zinput
+        z_log_var = zinput
+        for _ in range(n_dense_layers):
+            # represent the mean and variance branches
+            # with separate dense networks
+            z_mean = Dense(latent_dim,activation='relu')(z_mean)
+            z_log_var = Dense(latent_dim,activation='relu')(z_log_var)
+        
+        z = Lambda(vae_sample)([z_mean,z_log_var,latent_dim])
+
+        x1 = Dense(np.prod(input_shape[1:]),activation='relu')(z)
+        dense_out = Reshape(input_shape[1:])(x1)
+
+        conv_1 = Conv2D(n_filters,(nConv,nConv),padding="same", kernel_initializer = kernel_initializer)(dense_out)
+        act_1 = Activation("relu")(conv_1)
+
+        conv_2 = Conv2D(n_filters,(nConv,nConv),padding="same", kernel_initializer = kernel_initializer)(act_1)
+        act_2u = Activation("relu")(conv_2)
+    
+    
+    # return the result to the next layer up
+    return act_2u
+
+def vae_sample(args):
+    # random sample for VAE resampling latent space
+    z_mean,z_log_var,latent_dim = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0],latent_dim),
+                    mean=0., stddev=1.)
+    return z_mean + epsilon*K.exp(z_log_var)
 
 def filtered_mae(y_true,y_pred):
     """
