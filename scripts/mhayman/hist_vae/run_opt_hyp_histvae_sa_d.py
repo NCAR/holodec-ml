7c0e93c2ae3cb25bd6cc6c7f870fddf19d7b84ab
Matthew Hayman <mhayman@ucar.edu>
2021-04-28 14:11:27 -0600

    run script for self attention processor

---
+++ scripts/mhayman/hist_vae/run_opt_hyp_histvae_sa_d.py
@@ -0,0 +1,42 @@
+"""
+Train DenseNet to estimate PSD on Casper
+"""
+
+paths = {   'load_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/hist-from-vae/',
+            'save_data':'/glade/scratch/mhayman/holodec/holodec-ml-data/hist-from-vae/'}
+
+settings = {
+            'data_file': 'training_1_25particles_gamma.nc', #'histogram_training_data_5000count20200818T100751.nc',# 'histogram_training_data_5000count20200814T075733.nc',     # training data file
+            'validation_file':'validation_1_25particles_gamma.nc',
+            'test_file':'test_1_25particles_gamma_numpy.nc',
+            'scale_inputs':True, # Scale the inputs
+            'scale_labels':False, # Scale the labels - depending on loss function, may not be desirable
+            'output_activation':'exponential', # output activation function,
+            'valid_fraction':0.1,   # fraction of data reserved for validation
+            'test_fraction':0.1,    # fraction of data reserved for training
+            'loss_function':'cum_mse',     # loss function 'cum_poisson', 'kstest'
+            'log_input':False,     # include log of the input data as input
+            'early_stopping':True,  # implement early stopping
+            'h_chunk':128,      # xarray chunk size when loading
+            'holo_examples':[1, 4, 9, 50, 53, 77, 91, 101, 105, 267, 4593, 7548],  # example outputs
+
+            # optimization definitions: high, low and initial value
+            'learning_rate':[1e-4,1e-2,1e-3], 
+            'num_dense_layers':[0,10,1],
+            'num_input_nodes':[1,4096,64],
+            'num_dense_nodes':[1,4096,32],
+            'activation':['relu', 'sigmoid','relu'], # categorical list, last is initial value
+            'batch_size':[16,256,32],
+            'adam_decay':[1e-6,1e-2,1e-3],
+            'epoch_count':[10,200,100],
+            'embedding_size':[16,256,64],
+
+            # gp_minimize arguments
+            'n_calls':12,
+            'noise':0.05,
+            'n_jobs':-1,
+            'kappa':5
+            }
+
+exec_file = 'optimize_histvae_atten_d_hyperparam.py'
+exec(compile(open(exec_file,'rb').read(),exec_file,'exec'))
