15f6faf76095aa4fa7fdf81fddeb0e6742e32078
ggantos <ggantos@ucar.edu>
2021-05-25 13:01:18 -0600

    zd model commits for post jnet

---
+++ scripts/ggantos/qsub_zd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+# +
+#PBS -N zd
+#PBS -A NAML0001
+#PBS -l select=1:ncpus=8:ngpus=1:mem=256GB
+#PBS -l walltime=7:59:00
+#PBS -q casper
+
+#PBS -j eo
+#PBS -k eod
+#PBS -m a
+#PBS -M ggantos@ucar.edu
+
+module load ncarenv/1.3 gnu/8.3.0 openmpi/3.1.4 python/3.7.5 cuda/10.1
+source ~/.bashrc
+conda activate micro
+
+# -
+
+python -u train_zd.py ../../config/zd_1to25.yml
